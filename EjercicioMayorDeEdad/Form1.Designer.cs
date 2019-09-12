a0c0eccc8aa4934d959ae9dc20e254adac2bc153
Santiago Penagos Uribe <santipuribe@gmail.com>
2019-09-12 00:12:19 -0500

    Calcular edad con fechas de año y saber si tiene o no mayoria de edad

---
+++ EjercicioMayorDeEdad/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace EjercicioMayorDeEdad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.lbledad = new System.Windows.Forms.Label();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.btnaceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(69, 31);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(270, 39);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Mayoria de edad";
+            // 
+            // lbledad
+            // 
+            this.lbledad.AutoSize = true;
+            this.lbledad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbledad.Location = new System.Drawing.Point(72, 111);
+            this.lbledad.Name = "lbledad";
+            this.lbledad.Size = new System.Drawing.Size(133, 20);
+            this.lbledad.TabIndex = 1;
+            this.lbledad.Text = "Ingrese su edad:";
+            // 
+            // txtedad
+            // 
+            this.txtedad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtedad.Location = new System.Drawing.Point(239, 108);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(100, 27);
+            this.txtedad.TabIndex = 2;
+            // 
+            // btnaceptar
+            // 
+            this.btnaceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnaceptar.Location = new System.Drawing.Point(152, 170);
+            this.btnaceptar.Name = "btnaceptar";
+            this.btnaceptar.Size = new System.Drawing.Size(106, 36);
+            this.btnaceptar.TabIndex = 3;
+            this.btnaceptar.Text = "Aceptar";
+            this.btnaceptar.UseVisualStyleBackColor = true;
+            this.btnaceptar.Click += new System.EventHandler(this.Btnaceptar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 444);
+            this.Controls.Add(this.btnaceptar);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.lbledad);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Label lbledad;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.Button btnaceptar;
+    }
+}
+
---
+++ EjercicioMayorDeEdad/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioMayorDeEdad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnaceptar_Click(object sender, EventArgs e)
+        {
+            int edad = int.Parse(txtedad.Text);
+            if ( edad >=18) {
+                MessageBox.Show("Es usted mayor de edad"); 
+            }
+            else
+            {
+                MessageBox.Show("Es usted aún menor de edad");
+            }
+        }
+    }
+}
